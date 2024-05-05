94b82200485877d532bf93fd7fd76e7de8faabfe
MaouAqim <maouaqim@gmail.com>
2024-05-05 17:27:37 +0700

    first commit

---
+++ image.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+background = pygame.image.load(os.path.abspath("image/bg.png"))
+bullet=pygame.image.load(os.path.join("image","rocketlauncher.png"))
+player=pygame.image.load(os.path.join("image","GalGuard.png"))
+ufo=pygame.image.load(os.path.join("image","StarSents.png"))
+button=pygame.image.load(os.path.join("image","ammo.png"))
+alienBoss=pygame.image.load(os.path.join("image","GalBoss.png"))
+heart=pygame.image.load(os.path.join("image","heart.png"))
